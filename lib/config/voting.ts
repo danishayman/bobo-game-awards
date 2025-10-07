2e062faeab34f2399d8fc303f2574c1e3ab097db
danishayman <danishaiman3b@gmail.com>
2025-10-08 01:40:42 +0800

    change open date

--- lib/config/voting.ts
+++ lib/config/voting.ts
@@ -15,8 +15,8 @@ export const VOTING_CONFIG = {
   // Live voting start time (voting is locked until this time)
   // Only admins can vote before this time
   LIVE_VOTING_START_YEAR: 2025,
-  LIVE_VOTING_START_MONTH: 10, // September (1-12)
-  LIVE_VOTING_START_DAY: 1,
+  LIVE_VOTING_START_MONTH: 12, // September (1-12)
+  LIVE_VOTING_START_DAY: 25,
   LIVE_VOTING_START_HOUR: 0, // 6 PM
   LIVE_VOTING_START_MINUTE: 0,
   LIVE_VOTING_START_SECOND: 0,
